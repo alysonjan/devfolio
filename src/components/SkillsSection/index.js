5bf283c5fceed724500ad737e866e261e1698c1d
alysonjan <alysonjan@gmail.com>
2022-04-01 22:30:19 +0800

    refactor: omit skill bar progress

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Line, Title, TitleContainer } from "../shared/SharedStyles";
+import React from 'react'
+import { Line, Title, TitleContainer } from '../shared/SharedStyles'
 import {
   Bar,
   Image,
@@ -8,7 +8,7 @@ import {
   SkillsContainer,
   SkillsWrapper,
   SkillWrapper,
-} from "./skillsStyles";
+} from './skillsStyles'
 
 const SkillsSection = ({ id, title, frontend, api, backend }) => {
   return (
@@ -20,44 +20,44 @@ const SkillsSection = ({ id, title, frontend, api, backend }) => {
       <SkillsWrapper>
         {frontend.map((item, index) => {
           return (
-              <SkillWrapper key={index}>
-                <SkillImageContainer width="8rem" height="8rem">
-                  <Image src={item.img} alt={item.alt} />
-                </SkillImageContainer>
-                <SkillbarContainer>
+            <SkillWrapper key={index}>
+              <SkillImageContainer width="8rem" height="8rem">
+                <Image src={item.img} alt={item.alt} />
+              </SkillImageContainer>
+              {/* <SkillbarContainer>
                   <Bar width={item.percentage} color={item.background} />
-                </SkillbarContainer>
-              </SkillWrapper>
-          );
+                </SkillbarContainer> */}
+            </SkillWrapper>
+          )
         })}
       </SkillsWrapper>
       <SkillsWrapper>
         {api.map((item, index) => {
           return (
-              <SkillWrapper key={index}>
-                <SkillImageContainer width="19.5rem" height="5.5rem" isApi>
-                  <img src={item.img} alt={item.alt} />
-                </SkillImageContainer>
-              </SkillWrapper>
-          );
+            <SkillWrapper key={index}>
+              <SkillImageContainer width="19.5rem" height="5.5rem" isApi>
+                <img src={item.img} alt={item.alt} />
+              </SkillImageContainer>
+            </SkillWrapper>
+          )
         })}
       </SkillsWrapper>
       <SkillsWrapper>
         {backend.map((item, index) => {
           return (
-              <SkillWrapper key={index}>
-                <SkillImageContainer width="8rem" height="7.8rem" isBackend>
-                  <Image src={item.img} alt={item.alt} />
-                </SkillImageContainer>
-                <SkillbarContainer>
+            <SkillWrapper key={index}>
+              <SkillImageContainer width="8rem" height="7.8rem" isBackend>
+                <Image src={item.img} alt={item.alt} />
+              </SkillImageContainer>
+              {/* <SkillbarContainer>
                   <Bar width={item.percentage} color={item.background} />
-                </SkillbarContainer>
-              </SkillWrapper>
-          );
+                </SkillbarContainer> */}
+            </SkillWrapper>
+          )
         })}
       </SkillsWrapper>
     </SkillsContainer>
-  );
-};
+  )
+}
 
-export default SkillsSection;
+export default SkillsSection
